c883aba74db52fa24a58611c01e6894e2939ea23
gouravchahal0307 <gouravchahal0307@gmail.com>
2022-05-28 16:17:45 +0530

    Initial Commit

---
+++ jj/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  jj
+//
+//  Created by Gourav on 28/05/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
